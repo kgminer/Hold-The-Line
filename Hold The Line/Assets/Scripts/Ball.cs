391b6e52d357402377857e279535c07c502f763c
TJeffersonDavis <t_davis@knights.ucf.edu>
2019-02-23 16:37:10 -0500

    Added basic map, paddle, and ball objects and scripts

---
+++ Hold The Line/Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    Rigidbody ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+        ball.velocity = Vector3.one * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
