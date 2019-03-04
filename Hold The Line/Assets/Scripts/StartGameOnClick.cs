48d24fb4956d99142d3696344fc94fa3c2e1f7ec
kgminer <k.gerhart@comcast.net>
2019-03-03 19:34:22 -0500

    Updating Menu UI/Connecting Menu to Game

---
+++ Hold The Line/Assets/Scripts/StartGameOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameOnClick : MonoBehaviour
+{
+    public void StartByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    
+}
