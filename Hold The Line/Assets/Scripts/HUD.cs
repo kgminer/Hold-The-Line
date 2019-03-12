4c09745b90ca9e09df98ea9f3c03af7759051312
kgminer <k.gerhart@comcast.net>
2019-03-12 12:44:31 -0400

    Created HUD class

---
+++ Hold The Line/Assets/Scripts/HUD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public GameObject GameOverPanel;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OpenPausePanel()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ClosePausePanel()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void OpenGameOverPanel()
+    {
+        GameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
