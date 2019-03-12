2dda222659c07425a6875ea353035211c725e393
Tim Smyth <tim.smyth@knights.ucf.edu>
2019-03-11 22:19:00 -0400

    Added pause functionality
    Added pause functionality when escape is pressed during a game. Also began the game over functionality.

--- Hold The Line/Assets/Scripts/GameManager.cs
+++ Hold The Line/Assets/Scripts/GameManager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject PausePanel;
+    public GameObject GameOverPanel;
+
+    private bool isPaused = false;
+    private int activePlayers = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
@@ -17,13 +21,58 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            PauseGame();
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
         }
+
+        /*
+        activePlayers = checkActivePlayers();
+
+        if (activePlayers == 1)
+        {
+            GameOver();
+        }
+        */
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
     }
 
     void PauseGame()
     {
+        isPaused = true;
         PausePanel.SetActive(true);
         Time.timeScale = 0f;
     }
+
+    void GameOver()
+    {
+        Time.timeScale = 0f;
+        GameOverPanel.SetActive(true);
+    }
+
+    // TODO
+    // Until we have some way to check if a player has been eliminated.
+    /*
+    int checkActivePlayers()
+    {
+        int result = 0;
+
+        for (all players)
+            if (player is alive)
+                result++;
+
+        return result;
+    }
+    */
 }
