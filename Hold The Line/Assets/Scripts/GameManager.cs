89a3b601c8d8aacd52094d3d43731013a329864c
Tim Smyth <tim.smyth@knights.ucf.edu>
2019-03-11 20:28:12 -0400

    Added GameManager class

---
+++ Hold The Line/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
