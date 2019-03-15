dfea0a78c5500ecb1c6f490907aeb994a1661674
Luiz Alves <vinnyluiz4@gmail.com>
2019-03-15 11:40:32 -0400

    Script for defense walls

---
+++ Hold The Line/Assets/Scripts/Defense.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defense : MonoBehaviour
+{
+	Renderer defenseRenderer;
+	int counter = 0;
+
+    void Start()
+    {
+        // Get the GameObject's Renderer component
+        defenseRenderer = GetComponent<Renderer>();
+    }
+
+	void OnCollisionEnter(Collision impact) 
+	{
+		// Shows the amount of damage taken by the ball
+		if(impact.gameObject.name == "Ball")
+		{
+			switch(++counter)
+			{
+				case 1:
+					defenseRenderer.material.color = Color.green;
+					break;
+				case 2:
+					defenseRenderer.material.color = Color.yellow;
+					break;
+				case 3:
+					defenseRenderer.material.color = Color.red;
+					break;
+			}
+		}
+	}
+
+	void Update()
+	{
+		// This removes the defense wall on the 4th hit
+		if(counter == 4)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
