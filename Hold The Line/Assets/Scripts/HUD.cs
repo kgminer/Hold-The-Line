cf92a66f332f5306a07e8e3ff17ec6b39d44515d
kgminer <k.gerhart@comcast.net>
2019-03-18 12:50:20 -0400

    Renamed Game Objects/Added Player Text Functions in HUD

--- Hold The Line/Assets/Scripts/HUD.cs
+++ Hold The Line/Assets/Scripts/HUD.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
     public GameObject PausePanel;
     public GameObject GameOverPanel;
+    public GameObject TopLeftLabel;
+    public GameObject BottomLeftLabel;
+    public GameObject TopRightLabel;
+    public GameObject BottomRightLabel;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,5 +35,25 @@ public class HUD : MonoBehaviour
     {
         GameOverPanel.SetActive(true);
         Time.timeScale = 0f;
+    }
+
+    public void SetTopLeftLabelText(string inputText)
+    {
+        TopLeftLabel.GetComponent<Text>().text = inputText;
+    }
+
+    public void SetBottomLeftLabelText(string inputText)
+    {
+        BottomLeftLabel.GetComponent<Text>().text = inputText;
+    }
+
+    public void SetTopRightLabelText(string inputText)
+    {
+        TopRightLabel.GetComponent<Text>().text = inputText;
+    }
+
+    public void SetBottomRightLabelText(string inputText)
+    {
+        BottomRightLabel.GetComponent<Text>().text = inputText;
     }
 }
