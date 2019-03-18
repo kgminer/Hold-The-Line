aed95745a6614d393fcd77f55ab95ac9e0859b7d
Tim Smyth <tim.smyth@knights.ucf.edu>
2019-03-18 17:23:34 -0400

    Start button on the controller pauses the game

--- Hold The Line/Assets/Scripts/GameManager.cs
+++ Hold The Line/Assets/Scripts/GameManager.cs
@@ -42,7 +42,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Start"))
         {
             HUD.OpenPausePanel();
         }
