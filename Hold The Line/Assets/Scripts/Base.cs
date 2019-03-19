7ab8963bc2f0dcaad6e4479f4cb729cd752dd66a
kgminer <k.gerhart@comcast.net>
2019-03-18 20:17:27 -0400

    Added Bases/Logic - Story 7 Done

---
+++ Hold The Line/Assets/Scripts/Base.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Ball")
+        {
+            this.gameObject.SetActive(false);
+            GameManager.activePlayers--;
+        }
+    }
+}
