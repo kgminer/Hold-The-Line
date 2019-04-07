5d8f850fd913e795c3d0c609d6d3f7e946255572
kgminer <k.gerhart@comcast.net>
2019-04-07 18:50:01 -0400

    Redoing Pause Panel Menu Button/Build Quit

--- Hold The Line/Assets/Scripts/QuitOnClick.cs
+++ Hold The Line/Assets/Scripts/QuitOnClick.cs
@@ -6,6 +6,7 @@ public class QuitOnClick : MonoBehaviour
 {
     public void Quit()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+        //UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
     }
 }
