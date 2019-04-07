dcc7fa2619c59338b9abd8eef84903281b2197c3
Luiz Alves <vinnyluiz4@gmail.com>
2019-04-07 16:00:12 -0400

    Just changes the defense color walls to more fit the theme

--- Hold The Line/Assets/Scripts/Defense.cs
+++ Hold The Line/Assets/Scripts/Defense.cs
@@ -6,11 +6,15 @@ public class Defense : MonoBehaviour
 {
 	Renderer defenseRenderer;
 	private int counter = 0;
+	private Color newColor;
 
     void Start()
     {
         // This allows us to change the defense wall's color
         defenseRenderer = GetComponent<Renderer>();
+		
+		// To change the stating color
+		//defenseRenderer.material.color = Color.this[0,0,0,0];
     }
 
 	void OnCollisionEnter(Collision impact) 
@@ -34,13 +38,25 @@ public class Defense : MonoBehaviour
 			switch(++counter)
 			{
 				case 1:
-					defenseRenderer.material.color = Color.green;
+					newColor.a = 1.0f;
+					newColor.b = 0.65f;
+					newColor.g = 0.65f;
+					newColor.r = 0.65f;
+					defenseRenderer.material.color = newColor;
 					break;
 				case 2:
-					defenseRenderer.material.color = Color.yellow;
+					newColor.a = 1.0f;
+					newColor.b = 0.35f;
+					newColor.g = 0.35f;
+					newColor.r = 0.35f;
+					defenseRenderer.material.color = newColor;
 					break;
 				case 3:
-					defenseRenderer.material.color = Color.red;
+					newColor.a = 1.0f;
+					newColor.b = 0.1f;
+					newColor.g = 0.1f;
+					newColor.r = 0.1f;
+					defenseRenderer.material.color = newColor;
 					break;
                 default:
                     break;
